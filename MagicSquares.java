d9eaabe4bd8fcf34ded856af75d9a3ee245e6e0a
bordis <bordignonjm@gmail.com>
2025-09-21 15:19:08 +0100

    n queens

--- MagicSquares.java
+++ MagicSquares.java
@@ -1,20 +1,74 @@
 // this code is a test to solve the problem using brute force
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class MagicSquares {
+
+    public static int size = 3;
+
     public static void main(String[] args) {
 
-        long inicio = System.currentTimeMillis();
+        Scanner sc = new Scanner(System.in);
 
         int interactions = 0;
 
-        System.out.println("Init the N Queens puzzle");
+        // System.out.print("Enter the size of the board: ");
+        // size = sc.nextInt();
+        long inicio = System.currentTimeMillis();
 
+        System.out.println("Init the Magic Square puzzle N = " + size);
 
+        int[][] board = generateBoard();
+
+        printBoard(board);
 
         long fim = System.currentTimeMillis();
         long duracao = fim - inicio;
-        System.out.println("Execution time: " + duracao + " ms");
+        double segundos = duracao / 1000.0;
+
+        System.out.printf("Execution time: %.2f s%n", segundos);
         System.out.println("It was necessary " + interactions + " interactions!");
 
+        sc.close();
+
+    }
+
+    public static int[][] generateBoard() {
+        int[][] board = new int[size][size];
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= size * size; i++) {
+            numbers.add(i);
+        }
+        Collections.shuffle(numbers);
+        int indVector = 0;
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                board[x][y] = numbers.get(indVector);
+                indVector++;
+            }
+        }
+        return board;
+    }
+
+    // function to print the board
+    public static void printBoard(int[][] board) {
+        int size = board.length;
+        int maxNumber = 0;
+        for (int[] row : board) {
+            for (int num : row) {
+                if (num > maxNumber)
+                    maxNumber = num;
+            }
+        }
+        int width = String.valueOf(maxNumber).length() + 1;
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                System.out.printf("%" + width + "d", board[x][y]);
+            }
+            System.out.println();
+        }
     }
 }
--- Queens.java
+++ Queens.java
@@ -1,20 +1,114 @@
 // this code is a test to solve the problem using brute force
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Queens {
 
+    public static int size;
+    public static int interactions = 0;
+
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the board: ");
+        size = sc.nextInt();
+
+        System.out.println("The size going to be: " + size);
+
         long inicio = System.currentTimeMillis();
-        int rep = 0;
         System.out.println("Init the N Queens puzzle");
 
+        int[][] board = generateQuenns();
 
+        while (true) {
+            if (!validateBoard(board)) {
+                board = generateQuenns();
+            } else {
+                break;
+            }
+        }
+
+        printBoard(board);
 
         long fim = System.currentTimeMillis();
         long duracao = fim - inicio;
-        System.out.println("Execution time: " + duracao + " ms");
-        System.out.println("It was necessary " + rep + " interactions!");
+        double segundos = duracao / 1000.0;
 
+        System.out.printf("Execution time: %.2f s%n", segundos);
+        System.out.println("It was necessary " + interactions + " interactions!");
+
+        sc.close();
+
+    }
+
+    // function to validate the board
+    public static boolean validateBoard(int[][] board) {
+        interactions++;
+        // not allow queens in the same line
+        Set<Integer> line = new HashSet<>();
+        int[] positions = new int[size];
+        for (int x = 0; x < size; x++) {
+            int quennLine = 0;
+            for (int y = 0; y < size; y++) {
+                if (board[x][y] == 1) {
+                    quennLine = y;
+                }
+            }
+            // quick check, HashSet dont allow sames values, so its must be one queen in diferente line position
+            if (!line.add(quennLine)) {
+                return false;
+            } else {
+                positions[x] = quennLine;
+            }
+        }
+        // for all the quenns
+        // dont compare with the last queen, already compare int the loop
+        for (int i = 0; i < positions.length - 1; i++) {
+            // get the quem possition of the +1 i, to not compare with the same queen
+            for (int j = i + 1; j < positions.length; j++) {
+                // do the calc line x position, if the result is equal is in a invalid position
+                // first contidion have the absolute of the line and second of the columm
+                if (Math.abs(i - j) == Math.abs(positions[i] - positions[j])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // funtion to generate random quenns
+    public static int[][] generateQuenns() {
+        int[][] board = new int[size][size];
+        for (int x = 0; x < size; x++) {
+            Random rand = new Random();
+            int quenn = rand.nextInt(size);
+            for (int y = 0; y < size; y++) {
+                if (y == quenn) {
+                    board[x][y] = 1;
+                }
+            }
+        }
+        return board;
+    }
+
+    // function to print the board
+    public static void printBoard(int[][] board) {
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                if (board[x][y] == 1) {
+                    System.out.print("  x");
+                } else {
+                    System.out.print("  o");
+                }
+
+            }
+            System.out.println();
+        }
     }
 
 }
